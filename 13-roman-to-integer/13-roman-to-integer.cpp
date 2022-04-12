7c5ffbde47c0c042c038cca7b09ceb989c6b20fd
Abhinav Katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-04-12 18:39:07 +0530

    Time: 15 ms (58.22%), Space: 9.4 MB (6.14%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,59 +1,60 @@
-class Solution {
+class Solution
+{
 public:
-    int romanToInt(string s) {
+    int romanToInt(string s)
+    {
         int arr[s.length()];
-        for(int i = 0 ; i<s.length(); i++){
-            if(s[i] == 'M'){
-                arr[i]=1000;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == 'M')
+            {
+                arr[i] = 1000;
             }
-            if(s[i] == 'D'){
-                arr[i]=500;
+            if (s[i] == 'D')
+            {
+                arr[i] = 500;
             }
-            
-            if(s[i] == 'C'){
-                arr[i]=100;
+
+            if (s[i] == 'C')
+            {
+                arr[i] = 100;
             }
-            if(s[i] == 'L'){
-                arr[i]=50;
+            if (s[i] == 'L')
+            {
+                arr[i] = 50;
             }
-            if(s[i] == 'X'){
-                arr[i]=10;
+            if (s[i] == 'X')
+            {
+                arr[i] = 10;
             }
-            if(s[i] == 'V'){
-                arr[i]=5;
+            if (s[i] == 'V')
+            {
+                arr[i] = 5;
             }
-            if(s[i] == 'I'){
-                arr[i]=1;
+            if (s[i] == 'I')
+            {
+                arr[i] = 1;
             }
         }
-        stack<int>st;
+        stack<int> st;
         st.push(arr[0]);
-        int add= st.top()  ;
+        int add = st.top();
         int sub = 0;
-        for(int i = 1 ; i <s.length(); i++){
-            if(st.top()>=arr[i]){
+        for (int i = 1; i < s.length(); i++)
+        {
+            if (st.top() >= arr[i])
+            {
                 st.push(arr[i]);
-                add = arr[i] +add;
+                add = arr[i] + add;
             }
-            else{
+            else
+            {
                 add = add - st.top();
-                sub = arr[i]  -st.top();
+                sub = arr[i] - st.top();
                 st.push(arr[i]);
-                add = add+sub;
+                add = add + sub;
             }
         }
         return add;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
