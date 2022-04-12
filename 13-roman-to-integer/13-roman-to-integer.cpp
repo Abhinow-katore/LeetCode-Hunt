630ddd9cc01c82171bd30702bcfd3ca8c7590e4e
Abhinav Katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-04-12 18:40:37 +0530

    Time: 26 ms (25.14%), Space: 9.4 MB (6.14%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -6,35 +6,13 @@ public:
         int arr[s.length()];
         for (int i = 0; i < s.length(); i++)
         {
-            if (s[i] == 'M')
-            {
-                arr[i] = 1000;
-            }
-            if (s[i] == 'D')
-            {
-                arr[i] = 500;
-            }
-
-            if (s[i] == 'C')
-            {
-                arr[i] = 100;
-            }
-            if (s[i] == 'L')
-            {
-                arr[i] = 50;
-            }
-            if (s[i] == 'X')
-            {
-                arr[i] = 10;
-            }
-            if (s[i] == 'V')
-            {
-                arr[i] = 5;
-            }
-            if (s[i] == 'I')
-            {
-                arr[i] = 1;
-            }
+            if (s[i] == 'M')arr[i] = 1000;
+            if (s[i] == 'D')arr[i] = 500;
+            if (s[i] == 'C')arr[i] = 100;
+            if (s[i] == 'L')arr[i] = 50;
+            if (s[i] == 'X')arr[i] = 10;
+            if (s[i] == 'V')arr[i] = 5;
+            if (s[i] == 'I') arr[i] = 1;
         }
         stack<int> st;
         st.push(arr[0]);
