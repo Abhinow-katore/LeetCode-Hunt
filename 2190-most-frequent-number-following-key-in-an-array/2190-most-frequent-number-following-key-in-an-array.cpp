5fb46cb291bd7fe5a7a4aca6413fa78a05e4fba4
Abhinav Katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-04-12 15:05:57 +0530

    Time: 8 ms (91.80%), Space: 12.8 MB (35.28%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int mostFrequent(vector<int>& nums, int key) {
-        unordered_map<int, int> mp;
-
-    for(int i=0; i<nums.size()-1; i++){
-        if(nums[i]==key)
-            mp[nums[i+1]]++;    //increase target frequency
-    }
-    
-    int mx = INT_MIN;
-    int target = -1;
-    for(auto &it: mp){
-        if(it.second > mx){         //highest target count
-            mx = it.second;
-            target = it.first;     //store target(element)
+      unordered_map<int,int> umap; /*num, count*/
+      for(int i = 0; i <nums.size()-1; i++){
+        if(nums[i]==key){
+          umap[nums[i+1]]++;
         }
-    }
-    
-    return target;
+      }
+      
+      int maxE=-1;
+      int maxEC = 0;
+      for(auto it: umap){
+        if(it.second > maxEC){
+          maxE = it.first;
+          maxEC = it.second;
+        }
+      }
+      
+      return maxE;
+        
     }
 };
