9380f08cb3c5e5666d15682d5a2e261b59922013
Abhinav Katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-05-26 20:26:55 +0530

    Time: 3 ms (54.92%), Space: 5.8 MB (79.17%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int ans=0;
-        while(n>0){
-            if(n & 0b1) ans++;      
-            n=n>>1;
-        }
-        return ans;
+    //     int ans=0;
+    //     while(n>0){
+    //         if(n & 0b1) ans++;      
+    //         n=n>>1;
+    //     }
+    //     return ans;
+        return __builtin_popcount(n);
     }
 };
