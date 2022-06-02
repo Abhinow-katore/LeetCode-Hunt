0300b20bd0657b14d229a8e774feb4de0679e89a
Abhinav Katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-06-02 18:51:54 +0530

    Time: 33 ms (5.17%), Space: 10.6 MB (78.16%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -1,31 +1,13 @@
-// class Solution {
-// public:
-//     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-//         // int ans[matrix.size()][matrix[0].size()]; 
-//     vector<vector<int>> ans(matrix[0].size(), vector<int> (matrix.size(), 0));
-//       for(int i = 0 ; i<matrix.size(); i++){
-//           for(int j= 0 ; j<matrix[0].size(); j++){
-//               ans[j][i] = matrix[i][j];
-//           }
-//       }  
-//     return ans;
-//     }
-// };
-
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        int R = matrix.size();
-        int C = matrix[0].size();
-        
-        vector<vector<int>> ans(C, vector<int> (R,0));
-        
-        for (int i=0; i<R; i++) {
-            for(int j=0; j<C; j++) {
-                ans[j][i] = matrix[i][j];    
-            }
-        }
-        
-        return ans;
+        // int ans[matrix.size()][matrix[0].size()]; 
+    vector<vector<int>> ans(matrix[0].size(), vector<int> (matrix.size(), 0));
+      for(int i = 0 ; i<matrix.size(); i++){
+          for(int j= 0 ; j<matrix[0].size(); j++){
+              ans[j][i] = matrix[i][j];
+          }
+      }  
+    return ans;
     }
 };
