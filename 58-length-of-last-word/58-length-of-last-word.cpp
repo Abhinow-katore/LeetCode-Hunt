4eb7ed8eac161794618b404f2e17aec78fecdf42
Abhinav Katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-07-15 09:32:30 +0530

    Time: 4 ms (38.58%), Space: 6.5 MB (52.03%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -1,35 +1,12 @@
-// class Solution {
-// public:
-//     int lengthOfLastWord(string s) {
-//         int c = 0;
-//         int pos  = 0;
-//         if(s[s.length()-1]==' '){
-//             for(int i = s.length()-1;i>0;i--){
-//                 if(s[i]!=' ')break;
-//                 pos++;
-//             }
-//         }
-//         // c= pos;
-//         for(int i = s.length()-1-pos;i>0;i--){
-         
-//             if(s[i]==' ')break;
-//                c++;
-//             // cout<<s[i]<<" ";
-//         }
-//         // cout<<s.length();
-//         if(s.length() == c+1)return c+1;
-//         return c;
-//     }
-// };
+
 class Solution {
-    public:
-        inline int lengthOfLastWord(const string s) const {
-            short len = s.length() - 1;
-            unsigned short ret = 1;
-            
-            while((len >= 0) && (s[len--] == ' '));
-            while((len >= 0) && (s[len--] != ' ')) ++ret;
-            
-            return ret;
+public:
+    int lengthOfLastWord(string s) {
+        int ans = 0;
+        for(int i=s.size()-1;i>=0;--i){
+            if(s[i] == ' ' && ans > 0) return ans;
+            if(s[i] != ' ') ans++;
         }
+        return ans;
+    }
 };
