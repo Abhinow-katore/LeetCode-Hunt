2445824f14442760560f4fc667267c1799313d71
Abhinav Katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-08-08 07:48:16 +0530

    Time: 334 ms (61.71%), Space: 10.6 MB (47.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -10,8 +10,6 @@ public:
                 
             }
         }
-        for(int i = 0; i<nums.size();i++){
-            cout<<dp[i]<<" ";}
         return *max_element(dp.begin(), dp.end());;
     }
 };
